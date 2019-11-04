0afdd2a88653fef95fbcc15644140a19164de7f8
minho park <riderkaman@gmail.com>
2019-11-04 11:04:44 +0900

    [박민호]  - 프로젝트 시작

---
+++ main/crawler.py
@@ -0,0 +1,13 @@
+'''
+맨날 잡플래닛 평점 검색해서 보는 거 귀찮아서 만듬
+'''
+
+import requests
+
+keyword = input('검색할 회사명을 입력해 주세요')
+
+url = 'https://www.jobplanet.co.kr/search?category=&query=' + keyword + '&_rs_act=index_new&_rs_element=main_search_bar'
+
+req = requests.get(url)
+
+print(req.text)
